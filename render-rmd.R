3ab201f7e36a96555a8055df1f2e6a45d4d778e8
Kao Lee Yang <klyang3@wisc.edu>
2019-11-22 10:59:01 -0600

    updated with grabbing list named "file names"

--- render-rmd.R
+++ render-rmd.R
@@ -4,4 +4,9 @@
 require("rmarkdown")
 require("tidyverse")
 
-render("surveys-report.Rmd")
+file_names <- commandArgs(trailingOnly = TRUE)
+
+render("surveys-report.Rmd",
+       output_dir = "reports",
+       params = list(file = file_names)) 
+
