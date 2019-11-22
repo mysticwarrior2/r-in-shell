371149dcbf774d8e35b623cad755cd1a98772250
Kao Lee Yang <klyang3@wisc.edu>
2019-11-22 11:15:32 -0600

    for loop for multiple inputs

--- render-rmd.R
+++ render-rmd.R
@@ -3,10 +3,22 @@
 
 require("rmarkdown")
 require("tidyverse")
+require("stringr")
 
 file_names <- commandArgs(trailingOnly = TRUE)
 
-render("surveys-report.Rmd",
-       output_dir = "reports",
-       params = list(file = file_names)) 
+for (file in file_names)
+{
 
+out_name <- str_split_fixed(string = file,
+                            pattern = "[/|.]",
+                            n = 3)  #this command is saying that strings will be 
+                                    #split at the dash, and period
+                                    #then these should be recognized as 3 parts
+out_name <- str_c(out_name[2], '.html')
+
+render("surveys-report.Rmd",
+       output_file = out_name,
+       output_dir = "reports",
+       params = list(file = file)) 
+}
